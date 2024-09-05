91ec4d2d75e855580d530531d1f7bcab2bfb83f5
Shriram Balagopal <sbalagopal@freshworks.com>
2024-09-05 13:42:18 +0530

    initialcommit

---
+++ javascript/hello.js
@@ -0,0 +1,166 @@
+/*let firstname="priyanka";
+let age=25;
+let marks=455.8;
+
+console.log('Name:',firstname);
+console.log('Age:'+age);
+console.log('Total Marks:'+marks);
+
+let EmployeeName="priya";
+let Designation="Senior software engineer";
+let Experience="4 years";
+let Salary="1000000 INR per month";
+
+console.log('Employee Name:',EmployeeName);
+console.log('Designation:',Designation);
+console.log('Experience:',Experience);
+console.log('salary:',Salary);
+
+let m=1
+let n=2
+let o=3
+let p=4
+let q=(1,2,3,4);
+
+console.log(m,n,o,p);
+
+let x=20;
+let y=10;
+
+let result= x+y;
+console.log("addition=",result);
+
+result=x-y;
+console.log("subration=", result);
+
+result=x*y;
+console.log("multiplication=",result);
+
+result=x/y;
+console.log("division=",result);
+
+result=x**y;
+console.log("power=",result);
+
+result=x++;
+let c= x++;
+console.log("a value after=",result);
+console.log("a value after=",c);
+
+result=x%y;
+console.log("reminder=",result);
+
+c=++x+y--;
+let d=++x+y--;
+console.log("a value before=",c);
+console.log("a value before=",d);
+
+let r=7;
+let pi=22/7;
+let a=(r*r*pi);
+console.log("radius=",a);
+
+let l=10;
+let b=10;
+
+result=l*b;
+console.log("Result="+''+result);
+
+let g=10;
+result=1/4*(1.732*g*g);
+console.log("Result=",result);
+
+let f=10;
+result=f*9/5+32;
+console.log("Result=",result);
+
+let k=10;
+result=k+273.15;
+console.log("Result=",result); 
+
+let a=5;
+let b=1;
+
+let c=a|b;
+console.log(c);
+
+c=~a;
+console.log(c);
+
+c=a&b;
+console.log(c);
+
+c=a^b;
+console.log(c);
+
+c=a<<b;
+console.log(c);
+
+c=a>>b;
+console.log(c);
+
+//first sum
+let p=10000;
+let r=5;
+let t=3;
+result=(r*p*t)/100;
+console.log(result);
+
+//second sum
+let a=3.14159;
+let o=14;
+result=(a*o*o);
+console.log(result);
+
+//third sum
+let A=150;
+let B=200;
+C=A+B;
+A=C-B;
+B=C-A;
+console.log("A="+A);
+console.log("B="+B);
+
+//forth sum
+let x=85;
+let y=90;
+let z=95;
+result=(x+y+z)/3;
+console.log(result);
+
+//fivth sum
+let P=5000;
+let R=6;
+let T=4;
+result=P*(1+R/100)**T-P;
+console.log(result);
+
+//sixth sum
+let sp=950;
+let cp=800;
+Profit=sp-cp;
+console.log(Profit);
+
+//seveth sum
+let l=50;
+let w=30;
+result=2*(l+w);
+console.log(result);
+
+//eighth sum
+let X=25;
+let Y=19;
+result=X&Y;
+console.log(result);
+
+//ninth sum
+let celsius=22;
+fahrenheit=(celsius*9/5);
+console.log(fahrenheit);
+
+//tenth sum
+let volume=3.14159;
+let s=5;
+let h=10;
+result=volume*r*r*h;
+console.log(result);*/
---
+++ javascript/if-else.js
@@ -0,0 +1,67 @@
+let marks=20;
+if(marks>80)
+{
+    console.log("Grade-A");
+}
+else if(marks>50)
+{
+    console.log("Grade-B");
+}
+else if(marks>1)
+{
+    console.log("Grade-C");
+}
+else
+{
+    console.log("error");
+}
+
+let char='a';
+if(char==='a' || char==='A')
+{
+    console.log("the character is a vowel");
+}
+
+else if(char==='e' || char==='E')
+{
+    console.log("the character is a vowel");
+}
+
+else if(char==='i' || char==='I')
+{
+    console.log("the character is a vowel");
+}
+
+else if(char==='o' || char==='O')
+{
+    console.log("the character is a vowel");
+}
+
+else if(char==='u' || char==='U')
+{
+    console.log("the character is a vowel");
+}
+
+else{
+    console.log("the character is a consant");
+}
+
+let age=10000;
+if(age<5){
+    console.log("free");
+}
+else if(age>=5 && age<=12){
+    console.log("10$")
+}
+
+else if(age>=13 && age<=64){
+    console.log("20$");
+}
+
+else if(age>=65){
+    console.log("15$")
+}
+
+else{
+    console.log("ticket is not free")
+}
